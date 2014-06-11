c64bcaa3c185b9721646ae6fccca2b685bae8831
Paul M. Jones <pmjones88@gmail.com>
2014-06-11 17:14:47 -0500

    specific factory methods

--- src/AuthFactory.php
+++ src/AuthFactory.php
@@ -1,12 +1,49 @@
 <?php
 namespace Aura\Auth;
 
+use PDO;
+
 class AuthFactory
 {
-    public function newInstance($type)
+    public function newInstance(AdapterInterface $adapter)
     {
-        $type = ucfirst(strtolower($type));
-        $adapter = "Aura\Auth\Adapter\\{$type}";
-        return new Auth(new $adapter, new Session);
+        return new Auth(
+            $adapter,
+            new Session('Aura\Auth\Auth'),
+            new Timer
+        );
+    }
+
+    public function newPdoInstance(
+        PDO $pdo,
+        $verifier_spec,
+        array $cols,
+        $from,
+        $where = null
+    ) {
+        if (is_string($verifier_spec)) {
+            $verifier = new Verifier\HashVerifier($verifier_spec);
+        } elseif (is_int($verifier_spec)) {
+            $verifier = new Verifier\PasswordVerifier($verifier_spec);
+        } elseif {
+            $verifier = $verifier_spec;
+        }
+
+        return $this->newInstance(new Adapter\PdoAdapter(
+            $pdo,
+            $verifier,
+            $cols,
+            $from,
+            $where
+        ));
+    }
+
+    public function newHtpasswdInstance($file)
+    {
+        $verifier = new Verifier\HtpasswdVerifier;
+        return $this->newInstance(new Adapter\HtpassdAdapter(
+            $file,
+            $verifier
+        ));
     }
 }
