488e025072dd4f60893006b6d0dc4c82c4088293
Hari KT <kthari85@gmail.com>
2014-06-13 01:25:26 +0530

    Add some docblocks

--- src/Verifier/VerifierInterface.php
+++ src/Verifier/VerifierInterface.php
@@ -1,7 +1,34 @@
 <?php
+/**
+ *
+ * This file is part of Aura for PHP.
+ *
+ * @package Aura.Auth
+ *
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ *
+ */
 namespace Aura\Auth\Verifier;
 
+/**
+ *
+ * Password Verifier
+ *
+ * @package Aura.Auth
+ *
+ */
 interface VerifierInterface
 {
+    /**
+     * 
+     * @param string $plaintext Plaintext
+     * 
+     * @param string $encrypted encrypted string
+     * 
+     * @param array $extra Optional array if used by verify
+     * 
+     * @return bool
+     * 
+     */
     public function verify($plaintext, $encrypted, array $extra = array());
 }
