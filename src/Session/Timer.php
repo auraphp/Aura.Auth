14eb493cba18daebff293ef976a32fb1bb40543c
Paul M. Jones <pmjones88@gmail.com>
2014-06-20 09:21:36 -0500

    move Timer to Session namespace

--- src/Session/Timer.php
+++ src/Session/Timer.php
@@ -8,7 +8,10 @@
  * @license http://opensource.org/licenses/bsd-license.php BSD
  *
  */
-namespace Aura\Auth;
+namespace Aura\Auth\Session;
+
+use Aura\Auth\Exception;
+use Aura\Auth\Status;
 
 /**
  *
